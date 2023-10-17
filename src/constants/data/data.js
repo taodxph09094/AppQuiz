faab974a93050412f1e074a2d1bd17ca474afb32
Tao Xuan Dang <taodx@Taos-MacBook-Pro.local>
2023-10-17 11:05:19 +0700

    .

---
+++ src/constants/data/data.js
@@ -0,0 +1,68 @@
+export const data = [
+  {
+    index: 1,
+    title: 'Chơi',
+    isCorrect: 1,
+    correct: [
+      {key: 1, value: 'a'},
+      {key: 2, value: 'b'},
+      {key: 3, value: 'c'},
+    ],
+    locked: false
+  },
+  {
+    index: 2,
+    title: 'Chơi 2',
+    isCorrect: 1,
+    correct: [
+      {key: 1, value: 'a'},
+      {key: 2, value: 'b'},
+      {key: 3, value: 'c'},
+    ],
+    locked: true
+  },
+  {
+    index: 3,
+    title: 'Chơi 3',
+    isCorrect: 1,
+    correct: [
+      {key: 1, value: 'a'},
+      {key: 2, value: 'b'},
+      {key: 3, value: 'c'},
+    ],
+    locked: true
+  },
+  {
+    index: 4,
+    title: 'Chơi 4',
+    isCorrect: 1,
+    correct: [
+      {key: 1, value: 'a'},
+      {key: 2, value: 'b'},
+      {key: 3, value: 'c'},
+    ],
+    locked: true
+  },
+  {
+    index: 5,
+    title: 'Chơi 5',
+    isCorrect: 1,
+    correct: [
+      {key: 1, value: 'a'},
+      {key: 2, value: 'b'},
+      {key: 3, value: 'c'},
+    ],
+    locked: true
+  },
+  {
+    index: 6,
+    title: 'Chơi 6',
+    isCorrect: 1,
+    correct: [
+      {key: 1, value: 'a'},
+      {key: 2, value: 'b'},
+      {key: 3, value: 'c'},
+    ],
+    locked: true
+  },
+];
